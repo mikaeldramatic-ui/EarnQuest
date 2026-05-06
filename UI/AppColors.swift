d2f0aeabe3a2b5ed6b89749a39de60760c4c4382
Micke <mikael.dramatic@gmail.com>
2026-05-06 09:14:41 +0200

    added AppCors and Color+Extensions for using hex decimals to layouts

---
+++ UI/AppColors.swift
@@ -0,0 +1,16 @@
+//
+//  AppColors.swift
+//  EarnQuest
+//
+//  Created by Mikael Engvall on 2026-05-06.
+//
+
+import SwiftUI
+
+struct AppColors {
+    
+    static let background = Color(hex: "81A6C6")
+    static let infoCard = Color(hex: "AACDDC")
+    static let actionCard = Color(hex: "F3E3D0")
+    static let logout = Color(hex: "DC7575")
+}
